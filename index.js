54b504d2eacd3a6b2ca7a7ecc64a50c33c081c6f
ShahbranKhan <ghulamshahbran@gmail.com>
2019-08-19 18:40:32 +0200

    add login

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const bodyParser = require("body-parser");
+const pkg = require("./package.json");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
+
+
+app.use(bodyParser.json());
 
 /////////////////////////////////////////////////////////////// Se connecter à la base de données
-mongoose.connect("mongodb://localhost:27017/auto-ecoles", {
+mongoose.connect("mongodb://localhost:27017/KoikilDatabase", {
   useNewUrlParser: true
 });
 
 /////////////////////////////////////////////////////////////// Inititialisation des classes
-
 const tab=[
   {
     "Dept": null,
@@ -102920,16 +102927,184 @@ app.post("/autoecole/create", async (req, res) => {
 
 });
 
-/* app.use(require('forest-express-mongoose').init({
-  modelsDir: __dirname + '/models',
-  envSecret: process.env.FOREST_ENV_SECRET,
-  authSecret: process.env.FOREST_AUTH_SECRET,
-  mongoose: require('models').mongoose,
-})); */
 
 
 
 /////////////////////////////////////////////////////////////// Fin des routes pour Category
+
+app.get("/", (req, resp) =>
+  resp.json({
+    name: pkg.name,
+    version: pkg.version
+  })
+);
+
+//creation des models users
+
+const User = mongoose.model("User", {
+  lastName: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: Number,
+    required: true
+  },
+  adress: {
+    type: String,
+    required: true
+  },
+  postCode: {
+    type: String,
+
+    required: true,
+    minlength: 2,
+    maxlength: 5
+  },
+  salt: {
+    type: String
+  },
+  hash: {
+    type: String
+  },
+  token: {
+    type: String
+  },
+  contract: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Contract"
+  },
+  // model Userupload pour le chargement de documents contrat, livret, Piece d'identite
+  userUpload: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Userupload"
+  }
+});
+
+// route inscription (create users)
+app.post("/signup", async (req, res) => {
+  try {
+    const {
+      password,
+      email,
+      name,
+      lastName,
+      postCode,
+      adress,
+      phoneNumber
+    } = req.body;
+    // on créer un token
+    // uid2 est package qui permet de générer des chaines de caractere aléatoires
+    const token = uid2(16);
+    // on créer un salt
+    const salt = uid2(16);
+    // on génère le hash (SHA256 est un autre algorythme de hash)
+    const hash = SHA256(password + salt).toString(encBase64);
+    // on sauvegarde en bdd username, token, salt et hash mais pas password !
+    const user = new User({
+      email,
+      token,
+      salt,
+      hash,
+      name,
+      lastName,
+      postCode,
+      phoneNumber,
+      adress
+    });
+    await user.save();
+    res.json({
+      email,
+      token
+    });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+// route login
+app.post('/login', async (req, res) => {
+  const { password, email } = req.body;
+  // on cherche l'utilisateur par son username
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(403).json({
+      error: 'Unvalid email/password',
+    });
+    return;
+  }
+  // on re-génère le hash à partir du mot de passe envoyé et du salt de l'utilisateur
+  const hash = SHA256(password + user.salt).toString(encBase64);
+  // si le hash correspond, le mot de passe est bon
+  if (hash !== user.hash) {
+    res.status(403).json({
+      error: 'Unvalid email/password',
+    });
+    return;
+  }
+  res.json({
+    email,
+    token: user.token,
+  });
+});
+
+// route read user
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Modif du user
+app.post("/updateUser", async (req, res) => {
+  try {
+    if (req.body.email) {
+      const user = await User.findOne({ email: req.body.email });
+      (user.lastName = req.body.lastName),
+        (user.name = req.body.name),
+        (user.password = req.body.password),
+        (user.phoneNumber = req.body.phoneNumber),
+        (user.adress = req.body.adress),
+        (user.postCode = req.body.postCode);
+
+      await user.save();
+      res.json({ message: "Updated" });
+    } else {
+      res.status(400).json({ message: "Missing parameter" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// delete user
+app.post("/deleteUser", async (req, res) => {
+  try {
+    if (req.body.email) {
+      const user = await User.findOne({ email: req.body.email });
+      await user.remove();
+      res.json({ message: "Removed" });
+    } else {
+      res.status(400).json({ message: "Missing id" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+
+
 
 app.listen(3000, () => {
   console.log("Server started");
