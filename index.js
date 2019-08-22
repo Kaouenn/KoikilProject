0079bc377047646049089ebdcaf86e6a5fdfc07a
ShahbranKhan <ghulamshahbran@gmail.com>
2019-08-22 09:59:10 +0200

    revert

--- index.js
+++ index.js
@@ -3,21 +3,14 @@ const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser");
 const pkg = require("./package.json");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
 const cors = require("cors");
 const formidableMiddleware = require("express-formidable");
 
-
 app.use(formidableMiddleware());
-
-const autoecole = require("./routes/autoecole");
-const user = require("./routes/user");
-
 app.use(cors());
-app.use(bodyParser.json());
-
-app.use(autoecole);
-app.use(user);
-
 
 require("./models/autoecolemodel");
 require("./models/usermodel");
@@ -32,9 +25,6 @@ app.use(user);
 app.use(upload);
 
 /////////////////////////////////////////////////////////////// Se connecter à la base de données
-
-// Se connecter à la base de données
-
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/KoikilDatabase",
   {
