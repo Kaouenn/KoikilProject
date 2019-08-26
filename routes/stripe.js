d2cef641c2babff6fbe787bf7dece3fceecbcf8d
Kaouenn <45761654+Kaouenn@users.noreply.github.com>
2019-08-26 17:29:36 +0200

    Update stripe.js

--- routes/stripe.js
+++ routes/stripe.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const createStripe = require("stripe");
 const cors = require("cors");
 /* Votre clé privée doit être indiquée ici */
-const stripe = createStripe("sk_test_FcQTuuEym2CYxtkcMdLAsoEg002eXGf3Eg");
+const stripe = createStripe("sk_test_YOUR_PRIVATE_API_KEY");
 
 router.use(cors());
 router.use(body.json());
