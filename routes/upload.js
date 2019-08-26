dfde3738e289aa6ee9071b5ccb421f95f3097823
regis <regis@MacBook-Pro-de-regis.local>
2019-08-26 13:10:09 +0200

    bdd autoecole version 2.0

--- routes/upload.js
+++ routes/upload.js
@@ -48,4 +48,13 @@ router.post("/upload", (req, res) => {
   }
 });
 
+router.get("/upload", async (req, res) => {
+  try {
+    // const users = await User.find();
+    // res.json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
